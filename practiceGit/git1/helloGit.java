9832092160da8806b7b80c4675d080a7c5d0e1fb
Praveen <praveendaniel24@gmail.com>
2025-10-18 14:42:34 +0530

    some issue

--- practiceGit/git1/helloGit.java
+++ practiceGit/git1/helloGit.java
@@ -1,8 +1,15 @@
 package git1;
 
 public class helloGit {
-
-	public static void main(String[] args) {
-		System.out.println("Hello, my name is Praveen");
+	
+	public static void main(String[] args) { 	
+		
+		
+		System.out.println("Testing new branch");
 	}
+	
+
+
+
+
 }
